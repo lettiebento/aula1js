7550c9b5764efce01cd6d9098728808a5537e6c8
Letícia Bento <165064276+lettiebento@users.noreply.github.com>
2025-06-25 20:17:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+alert ("Comando para mostrar um texto na tela, posso usar aspas duplas ou aspas simples, mas siga este padrão em todo código")
 
+alert ("Ele é limitado ao clique no ok pelo usuário")
+
+alert ("Não é possivel configurar seu tempo de permanência na tela")
