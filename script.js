519e7b1309f1505c8165bde77234fc592038f786
Letícia Bento <165064276+lettiebento@users.noreply.github.com>
2025-06-27 14:41:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 alert('Seja bem-vindo ao nosso jogo!');
+alert('Vamos ao calculo de média!');
 
-// Variável para armazenar o chute do usuári
-let chute;
-let tentativas = 0; // Inicia o contador de tentativas
-
-while (chute != numeroSecreto) {
-  chute = parseInt(prompt('Escolha um número entre 1 e 10'));
-  tentativas++; // Soma +1 a cada tentativa
-
-  if (chute == numeroSecreto) {
-    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} na tentativa ${tentativas}.`);
-  } else if (chute > numeroSecreto) {
-    alert(`O número secreto é menor que ${chute}. Tente novamente.`);
-  } else {
-    alert(`O número secreto é maior que ${chute}. Tente novamente.`);
-  }
+let tamanho;
+let nota;
+let media = 0;
+tamanho = parseInt(prompt('Quantas notas você quer inserir?'));
+for (x = 0; x < tamanho; x++) {
+    nota = parseFloat(prompt('Digite a nota ' + (x + 1) + ':'));
+    if (nota < 0 || nota > 10) {
+        alert('Nota inválida! Digite uma nota entre 0 e 10.');
+        x--; 
+    } else {
+        console.log('Nota ' + (x + 1) + ': ' + nota);
+    }
+    media += nota;
 }
+alert('Média: ' + (media / tamanho).toFixed(2));
